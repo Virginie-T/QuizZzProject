5ca051560cc3124dd311a54c20f2250adec7f8bb
virginie <virginietrubiano@gmail.com>
2014-12-09 15:33:39 -0800

    adapt for Iphone5 + sound JS

--- js/main.js
+++ js/main.js
@@ -10,16 +10,120 @@ $("#answers").click(function(){
 
 
 
-var sound = document.getElementsByTagName("audio")[0];
-
-sound.addEventListener(function() { document.getElementsByTagName("audio")[0].play()});
 
 
-$(".text-more-fun").click(function(event) {
-	 event.preventDefault();
-	$(this).addClass('rotate-text');
-	window.setTimeout("src='Video2.html'",500);
+/************************************SOUND*******************************/
+
+
+/*////////////////PLAY PAGE ////////*/
+
+/*logo*/
+
+$(document).ready(function(){
+    
+    $('.logo').on('click', function(event){
+        event.preventDefault(); 
+        
+
+        $('<audio></audio>')
+            .attr('src', 'http://www.freesfx.co.uk/rx2/mp3s/10/12016_1415549742.mp3')
+            .attr('autoplay', 'autoplay');  
+        
+        window.location.href = $(this).attr('href');  
+    });
+    
+
+/* anim principale */
+
+    
+    $('.anim-principal').on('click', function(event){
+        event.preventDefault(); 
+        
+
+        $('<audio></audio>')
+            .attr('src', 'http://www.freesfx.co.uk/rx2/mp3s/10/12016_1415549742.mp3')
+            .attr('autoplay', 'autoplay');  
+        
+        window.location.href = $(this).attr('href');  
+    });
+    
+/* Play  button */
+
+    
+    $('.banner').on('click', function(event){
+        event.preventDefault(); 
+        
+
+        $('<audio></audio>')
+            .attr('src', 'http://www.freesfx.co.uk/rx2/mp3s/10/12016_1415549742.mp3')
+            .attr('autoplay', 'autoplay');  
+        
+        window.location.href = $(this).attr('href');  
+    });
+    
+/*///////////////////VIDEO PAGE/////////*/
+
+/*guess button*/
+
+    
+    $('.guess-anim').on('click', function(event){
+        event.preventDefault(); 
+        
+
+        $('<audio></audio>')
+            .attr('src', 'http://www.freesfx.co.uk/rx2/mp3s/10/12016_1415549742.mp3')
+            .attr('autoplay', 'autoplay');  
+        
+        window.location.href = $(this).attr('href');  
+    });
+
+/*/////////ANSWERS PAGE //////////*/   
+
+/*banner*/
+
+    $('.wrong').on('click', function(event){
+        event.preventDefault(); 
+        
+
+        $('<audio></audio>')
+            .attr('src', 'http://www.freesfx.co.uk/rx2/mp3s/10/12016_1415549742.mp3')
+            .attr('autoplay', 'autoplay');  
+        
+        window.location.href = $(this).attr('href');  
+    });
+
+
+
+  $('.right').on('click', function(event){
+        event.preventDefault(); 
+        
+
+        $('<audio></audio>')
+            .attr('src', 'http://www.freesfx.co.uk/rx2/mp3s/10/12016_1415549742.mp3')
+            .attr('autoplay', 'autoplay');  
+        
+        window.location.href = $(this).attr('href');  
+    });
+
+/*/////////////WRONG_RESULT PAGE //////*/
+
+  $('#more-fun').on('click', function(event){
+        event.preventDefault(); 
+        
+
+        $('<audio></audio>')
+            .attr('src', 'http://www.freesfx.co.uk/rx2/mp3s/10/12016_1415549742.mp3')
+            .attr('autoplay', 'autoplay');  
+        
+        window.location.href = $(this).attr('href');  
+    });
+
+
+/************************************ANIMATION*******************************/
+
+    $(".text-more-fun").click(function(event) {
+	    event.preventDefault();
+	   $(this).addClass('rotate-text');
+    });
 
 });
-
-
